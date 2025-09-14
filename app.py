e8374db88613c63e9bdf576609f2508dc8967cb5
ShaneNum <150904595+shaneNum@users.noreply.github.com>
2025-09-14 13:38:52 +0700

    Batch 1

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content = task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Seems like there was an issue regarding naming input'
+
+    else:
+        tasks = Todo.query.order_by(Todo.id).all()
+        return render_template('index.html', tasks = tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Seems like there was an issue regarding deletion'
+
+if __name__ == "__main__":
+    app.run(debug=True)
